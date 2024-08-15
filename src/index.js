2dbfb8d7b2d87a20454c61294d5aa415b59a8a1b
Abraomukas <abraomukas@gmail.com>
2024-08-15 06:35:00 +0200

    wip: improve index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import './i18n';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const spinnerSizing = { width: '3rem', height: '3rem' };
+
+const spinnerFallback = (
+	<div className='d-flex justify-content-center mt-3'>
+		<div className='spinner-border' style={spinnerSizing} role='status'>
+			<span className='visually-hidden'>Loading...</span>
+		</div>
+	</div>
+);
+
+/**
+ * PAGES
+ */
+
+const router = createBrowserRouter([]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.Suspense fallback={spinnerFallback}>
+		<RouterProvider router={router} />
+	</React.Suspense>
 );
