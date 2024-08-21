000e71165ca8682cce9bdab5e1b9d586a56e3052
Abraomukas <abraomukas@gmail.com>
2024-08-21 12:35:00 +0200

    feat: add BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+/**
+ * PAGES
+ */
+import Main from './pages/Main';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' exact component={Main} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
