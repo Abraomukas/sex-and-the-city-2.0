97c3c9d389f10b0d0036b84fcba0d97923977fd8
Abraomukas <abraomukas@gmail.com>
2024-09-02 22:50:00 +0200

    feat: fix Blog's page layout

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { Trans, useTranslation } from 'react-i18next';
 
@@ -9,50 +10,60 @@ import Footer from '../components/Footer';
 export default function Blog() {
 	const currentLngCode = Cookies.get('i18next') || 'gb';
 
-	stories = [{ label: '', to: '/' }];
+	const stories = [{ label: '', to: '/' }];
 
 	return (
 		<div style={{ position: 'relative', minHeight: '100vh' }}>
 			<Navbar />
 
-			<div className='container'>
-				{/* HEADER */}
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+				}}>
+				<div className='container'>
+					{/* HEADER */}
 
-				<div className='position-relative p-5 p-md-1 m-md-1 text-center'>
-					<div className='col-md-6 p-lg-5 mx-auto mt-3'>
-						<h1 className='display-3 fw-bold'>
-							<Trans i18nKey={'careers.header'} />
-						</h1>
+					<div className='position-relative p-5 p-md-1 m-md-1 text-center'>
+						<div className='col-md-6 p-lg-5 mx-auto mt-3'>
+							<h1 className='display-3 fw-bold'>
+								<Trans i18nKey={'blog.header'} />
+							</h1>
+						</div>
 					</div>
-				</div>
 
-				{/* GRID */}
+					{/* GRID */}
 
-				<div className='container mb-7'>
-					<div className='row gx-3 d-flex justify-content-center'>
-						{stories.map((story, index) => {
-							return (
-								<div
-									key={index}
-									className='col-lg-4 col-md-12 mb-3 text-center'>
-									<div key={index}>
-										<Link key={index} to={`${story.label}`}>
-											<div key={index} className='text-bg-primary p-3'>
-												<h2
-													key={index}
-													className='display-5 d-flex align-items-center justify-content-center'
-													style={{ minHeight: '115px' }}>
-													<Trans key={index} i18nKey={''} />
-												</h2>
-											</div>
-										</Link>
+					<div className='container mb-7'>
+						<div className='row gx-3 d-flex justify-content-center'>
+							{stories.map((story, index) => {
+								return (
+									<div
+										key={index}
+										className='col-lg-4 col-md-12 mb-3 text-center'>
+										<div key={index}>
+											<Link key={index} to={`${story.label}`}>
+												<div key={index} className='text-bg-primary p-3'>
+													<h2
+														key={index}
+														className='display-5 d-flex align-items-center justify-content-center'
+														style={{ minHeight: '115px' }}>
+														<Trans key={index} i18nKey={''} />
+													</h2>
+												</div>
+											</Link>
+										</div>
 									</div>
-								</div>
-							);
-						})}
+								);
+							})}
+						</div>
 					</div>
 				</div>
 			</div>
+
+			<Footer />
 		</div>
 	);
 }
