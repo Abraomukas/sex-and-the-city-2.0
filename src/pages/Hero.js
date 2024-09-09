03e16ab90cad7fa7e89a6de71452caae09a3002a
Abraomukas <abraomukas@gmail.com>
2024-09-09 21:15:00 +0200

    feat: remove unused variable

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -5,12 +5,6 @@ import { Trans, useTranslation } from 'react-i18next';
 export default function Hero() {
 	const currentLngCode = Cookies.get('i18next') || 'gb';
 
-	const values = [
-		{ icon: 'fa-solid fa-person', tag: 'diversity', to: '' },
-		{ icon: 'fa-solid fa-users', tag: 'trust', to: '' },
-		{ icon: 'fa-solid fa-stopwatch', tag: 'productivity', to: '' },
-	];
-
 	return (
 		<div
 			style={{
