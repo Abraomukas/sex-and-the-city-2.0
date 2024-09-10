e5dab6091507df4df34d9cd5eb0acd6ff964b163
Abraomukas <abraomukas@gmail.com>
2024-09-10 23:15:00 +0200

    feat: update router in App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
  * PAGES
  */
 import Main from './pages/Main';
+import AboutMe from './pages/AboutMe';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' exact component={Main} />
+				<Route path='/about' exact component={AboutMe} />
 			</Routes>
 		</BrowserRouter>
 	);
