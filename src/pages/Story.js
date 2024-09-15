451585663bd78b9d78937fd31fb50d34bf6d6138
Abraomukas <abraomukas@gmail.com>
2024-09-15 23:20:00 +0200

    feat: add navbar and footer to Story.js

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -2,20 +2,29 @@ import React from 'react';
 import Cookies from 'js-cookie';
 import { Trans, useTranslation } from 'react-i18next';
 
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 export default function Story() {
 	const currentLngCode = Cookies.get('i18next') || 'gb';
 
 	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				height: '100vh',
-			}}>
-			<h3>
-				<Trans i18nKey={'story.header'} />
-			</h3>
+		<div style={{ position: 'relative', minHeight: '100vh' }}>
+			<Navbar />
+
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+				}}>
+				<h3>
+					<Trans i18nKey={'story.header'} />
+				</h3>
+			</div>
+
+			<Footer />
 		</div>
 	);
 }
